176e0540e363ee2ad6d857547cdde7e7bd414be7
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-09-09 19:32:05 +0200

    removed useless comment

--- 2021-Sep-07_Serverless_deployments_with_Terraform/main-2-iam-start-stop/step-1-iam-ec2-start-stop/create_access_rc.sh
+++ 2021-Sep-07_Serverless_deployments_with_Terraform/main-2-iam-start-stop/step-1-iam-ec2-start-stop/create_access_rc.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# LINE: {"id":"AKIAYIUDT6EGGMX4CPXO","secret":"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","user":"student-0"}
-
 #jq -c '.resources[0].instances[].attributes | { id, secret, user } '  terraform.tfstate  | grep student-0 | sed -e 's/{"id":/export AWS_ACCESS_KEY_ID=/' -e 's/","secret":"/\nexport AWS_SECRET_ACCESS_KEY=/' -e 's/","user.*//' > student0.rc
 for LINE in $( jq -c '.resources[0].instances[].attributes | { id, secret, user }' terraform.tfstate ); do
     #echo "LINE: '$LINE'"
